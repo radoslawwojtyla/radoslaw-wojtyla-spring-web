45a081d0c0efc27190e7faf4d3ed754205faf127
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-10-07 22:51:39 +0200

    Update on heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,11 +1,14 @@
 package com.crud.tasks;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Data
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
