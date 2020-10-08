84aef932de6c55df24e492318e11b0b1bf60787e
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-10-08 18:32:26 +0200

    proba 4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,7 +3,6 @@ package com.crud.tasks;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Data
 @SpringBootApplication
