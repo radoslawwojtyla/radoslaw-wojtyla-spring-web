aeb14cd56217a7e9691437920e23ec25c59820b4
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-10-15 10:36:18 +0200

    Conditional operator in scheduler

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -21,15 +21,12 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
     private static final String SUBJECT = "Tasks: Once a day email";
-    private String taskOrTasks = "tasks";
 
     @Scheduled(cron = "0 0 10 * * *")
 //    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
-        if (size == 1) {
-            taskOrTasks = "task";
-        }
+        String taskOrTasks = size == 1 ? "task" : "tasks";
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
